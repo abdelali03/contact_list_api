06bc8937192a19cb4def89adb14c625efda4c677
Your Name <you@example.com>
2024-10-26 02:56:24 +0100

    Updated CORS configuration to accept origin

--- src/main/java/io/elamine/contactapi/config/CorsConfig.java
+++ src/main/java/io/elamine/contactapi/config/CorsConfig.java
@@ -1,23 +1,15 @@
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://abdelalicontact.netlify.app"));
-        config.setAllowedHeaders(List.of("*"));  // This allows all headers
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Allow specified methods
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://abdelalicontact.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
-
