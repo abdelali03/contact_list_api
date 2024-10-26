d4ae70f0983c356de7599b6fa3a689261ad312b1
Your Name <you@example.com>
2024-10-26 03:30:12 +0100

    Updated CORS configuration to accept origin

--- src/main/java/io/elamine/contactapi/config/CorsConfig.java
+++ src/main/java/io/elamine/contactapi/config/CorsConfig.java
@@ -1,15 +1,72 @@
+package io.elamine.contactapi.config;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+import static io.elamine.contactapi.constant.Constant.X_REQUESTED_WITH;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpHeaders.ORIGIN;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
+/**
+ * @author Junior RT
+ * @version 1.0
+ * @license Get Arrays, LLC (<a href="https://www.getarrays.io">Get Arrays, LLC</a>)
+ * @email getarrayz@gmail.com
+ * @since 11/23/2023
+ */
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://abdelalicontact.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+public class CorsConfig {
+
+
+        @Bean
+        public CorsFilter corsFilter() {
+            var urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+            var corsConfiguration = new CorsConfiguration();
+            corsConfiguration.setAllowCredentials(true);
+            corsConfiguration.setAllowedOrigins(List.of("https://abdelalicontact.netlify.app"));  // Correct origins
+            corsConfiguration.setAllowedHeaders(List.of(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS));
+            corsConfiguration.setExposedHeaders(List.of(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION));
+            corsConfiguration.setAllowedMethods(List.of(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name()));
+            urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+            return new CorsFilter(urlBasedCorsConfigurationSource);
+        }
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/io/elamine/contactapi/resource/ContactResource.java
+++ src/main/java/io/elamine/contactapi/resource/ContactResource.java
@@ -18,13 +18,7 @@ import static io.elamine.contactapi.constant.Constant.PHOTO_DIRECTORY;
 import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
-/**
- * @author Junior RT
- * @version 1.0
- * @license Get Arrays, LLC (<a href="https://www.getarrays.io">Get Arrays, LLC</a>)
- * @email getarrayz@gmail.com
- * @since 11/22/2023
- */
+
 
 @RestController
 @RequestMapping("/contacts")
@@ -32,32 +26,32 @@ import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 public class ContactResource {
     private final ContactService contactService;
 
-    @CrossOrigin(origins = "http://192.168.56.1:3000")
+    @CrossOrigin(origins = "https://abdelalicontact.netlify.app")
     @PostMapping
     public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
         //return ResponseEntity.ok().body(contactService.createContact(contact));
         return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
     }
 
-    @CrossOrigin(origins = "http://192.168.56.1:3000")
+    @CrossOrigin(origins = "https://abdelalicontact.netlify.app")
     @GetMapping
     public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue = "0") int page,
                                                      @RequestParam(value = "size", defaultValue = "10") int size) {
         return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
     }
-    @CrossOrigin(origins = "http://192.168.56.1:3000")
+    @CrossOrigin(origins = "https://abdelalicontact.netlify.app")
     @GetMapping("/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable(value = "id") String id) {
         return ResponseEntity.ok().body(contactService.getContact(id));
     }
-    @CrossOrigin(origins = "http://192.168.56.1:3000")
+    @CrossOrigin(origins = "https://abdelalicontact.netlify.app")
     @PutMapping("/photo")
     public ResponseEntity<String> uploadPhoto(@RequestParam("id") String id, @RequestParam("file")MultipartFile file) {
         return ResponseEntity.ok().body(contactService.uploadPhoto(id, file));
     }
 
 
-    @CrossOrigin(origins = "http://192.168.56.1:3000")
+    @CrossOrigin(origins = "https://abdelalicontact.netlify.app")
     @GetMapping(path = "/image/{filename}", produces = { IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE })
     public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
         return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
@@ -67,7 +61,7 @@ public class ContactResource {
         return ResponseEntity.ok().build(); // Just responds with 200 OK
     }
 
-    @CrossOrigin(origins = "http://192.168.56.1:3000")
+    @CrossOrigin(origins = "https://abdelalicontact.netlify.app")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteContact(@PathVariable String id) {
         try {
